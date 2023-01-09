b34bfe4307973f7fa92c6c2c0b1b875a214ff576
Kamryn Smith <programersign@gmail.com>
2023-01-09 09:54:45 -0800

    commit tree 9b03aaee4978de318300ebf0969a421993b8af48

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// Variables:
+const employee = {
+    name: "David",
+    streetAddress: "123 Object Lane",
+};
+
+
+
+//Functions:
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
